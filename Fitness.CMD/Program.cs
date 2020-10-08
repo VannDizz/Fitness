9d69bfdf27a98cf7916952018a923678592922a2
Yuriy Klobukov <vanhoock@gmail.com>
2020-10-08 11:35:48 +0300

    Создана модель пользователя

---
+++ Fitness.CMD/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fitness.BL;
+using Fitness.BL.Model;
+
+namespace Fitness.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User user = new User("Yuriy",,,,);
+        }
+    }
+}
